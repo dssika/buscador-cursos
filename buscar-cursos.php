334a73215144aacbe9b199c089d7109a7514fb02
Jessica da Silva Santos Cunha <jessica.cunha@tjse.jus.br>
2025-07-14 13:15:52 -0300

    adiciona psr-4

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -2,34 +2,16 @@
 
 require 'vendor/autoload.php';
 
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['verify' => false]);
-
-$response = $client->request('GET', 'http://alura.com.br/cursos-online-programacao/php');
-
-$html = $response->getBody();
-
+$client = new Client(['verify' => false, 'base_uri' => 'http://alura.com.br']);
 $crawler = new Crawler();
-$crawler->addHtmlContent($html);
 
-$cursos = $crawler->filter('span.card-curso__nome');
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo $curso->textContent . PHP_EOL;
+    echo $curso. PHP_EOL;
 }
-
-
-
-// echo $response->getStatusCode(); // 200
-// echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
-// echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
-
-// // Send an asynchronous request.
-// $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
-// $promise = $client->sendAsync($request)->then(function ($response) {
-//     echo 'I completed! ' . $response->getBody();
-// });
-
-// $promise->wait();
